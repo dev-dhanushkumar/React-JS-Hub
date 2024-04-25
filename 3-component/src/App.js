744f8fd0ee47ee5b2b3f502a5d95c36db5cbfd2d
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-04-26 00:12:40 +0300

    class component are updated

---
+++ 3-component/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Class_Component from './Class-Component';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello App Here!</h1>
+      <Class_Component />
+    </div>
+  );
+}
+
+export default App;
---
+++ 3-component/src/Class-Component.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export default class Class_Component extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Class Component Here!</h1>
+            </div>
+        )
+    }
+}
