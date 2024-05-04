878d299d035d9fe1e4d699e305603837515b7fb4
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-05-04 22:10:08 +0300

    New file Component

---
+++ 37-router_setup/src/Nav.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+function Nav() {
+  <div>
+    <Link to="/">Home</Link>
+    <br /> <br />
+    <Link to="/about">About</Link>
+  </div>;
+}
+
+export default Nav;
