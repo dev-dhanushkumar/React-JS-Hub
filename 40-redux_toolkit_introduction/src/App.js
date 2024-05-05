06f6034502ae207119c4bcc28a1766abe030e51d
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-05-05 21:00:07 +0300

    update pending ...

---
+++ 40-redux_toolkit_introduction/src/App.js
@@ -0,0 +1,14 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <div className="App">
+      <Todos />
+      <h1 class="text-3xl font-bold underline bg-slate-400">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
