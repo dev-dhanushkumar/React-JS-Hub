0f5aa0dd94893e3583d963d169d4fec3b54ac7c7
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-10-09 07:58:01 +0530

    firebase config

---
+++ Projects🦁/CHAT-APPLICATION/src/config/firebase.js
@@ -0,0 +1,59 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+
+const firebaseConfig = {
+  // Write Fire base Key Here!
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+
+const signup = async (username, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await setDoc(doc(db, "users", user.uid), {
+            id: user.uid,
+            username: username.toLowerCase(),
+            email,
+            name:  "",
+            avatar:"",
+            bio: "Hey, There I am using chat App",
+            lastseen: Date.now()
+        })
+
+        await setDoc(doc(db,"chats", user.uid), {
+            chatData:[]
+        })
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const login = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+        
+    }
+}
+
+const logout = async () => {
+    try {
+        await signOut(auth);
+    } catch (error) {
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+export {signup, login, logout, auth, db}
