95f8e591bfab0c401fb0ad63b363f3ebf7e28f35
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-10-11 19:30:55 +0530

    App StateManagement Update

--- Projects🦁/CHAT-APPLICATION/src/context/AppContext.jsx
+++ Projects🦁/CHAT-APPLICATION/src/context/AppContext.jsx
@@ -9,15 +9,18 @@ const AppContextProvider = (props) => {
 
     const navigate = useNavigate(); 
 
-    const [userData, SetUserData] = useState(null);
+    const [userData, setUserData] = useState(null);
     const [chatData, setChatData] = useState(null);
+    const [messagesId, setMessagesId] = useState(null);
+    const [messages, setMessages] = useState([]);
+    const [chatUser, setChatUser] = useState(null);
 
     const loadUserData = async (uid) => {
         try {
             const userRef = doc(db,'users', uid);
             const userSnap = await getDoc(userRef);
             const userData = userSnap.data();
-            SetUserData(userData);
+            setUserData(userData);
             if(userData.avatar && userData.name) {
                 navigate('/chat');
             } else {
@@ -45,6 +48,7 @@ const AppContextProvider = (props) => {
             const chatRef = doc(db,'chats', userData.id);
             const unSub = onSnapshot(chatRef, async (res) => {
                 const chatItems = res.data().chatsData;
+                // console.log(res.data());
                 const tempData = [];
                 for(const item of chatItems) {
                     const userRef = doc(db, 'users', item.rId);
@@ -52,14 +56,20 @@ const AppContextProvider = (props) => {
                     const userData = userSnap.data();
                     tempData.push({...item, userData})
                 }
-                setChatData(tempData.sort((a,b) => b.updateAt - a.updateAt))
+                setChatData(tempData.sort((a,b) => b.updatedAt - a.updatedAt))
             })
+            return () => {
+                unSub();
+            }
         }
     },[userData])
     const value = {
-        userData, SetUserData,
+        userData, setUserData,
         chatData, setChatData,
-        loadUserData
+        loadUserData,
+        messages, setMessages,
+        messagesId, setMessagesId,
+        chatUser, setChatUser
     }
 
     return(
