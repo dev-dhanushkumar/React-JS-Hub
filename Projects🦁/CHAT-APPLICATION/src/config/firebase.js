6831b5e468076f9505a89470b7cf569e1f270ddd
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-10-11 19:30:28 +0530

    collection name change

--- Projects🦁/CHAT-APPLICATION/src/config/firebase.js
+++ Projects🦁/CHAT-APPLICATION/src/config/firebase.js
@@ -33,7 +33,7 @@ const signup = async (username, email, password) => {
     });
 
     await setDoc(doc(db, "chats", user.uid), {
-      chatData: [],
+      chatsData: [],
     });
   } catch (error) {
     console.log(error);
